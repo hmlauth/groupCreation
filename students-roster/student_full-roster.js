0eeb4356b250c943498226d8b8bbb23ba0de617d
Andromeda <hannalauth@gmail.com>
2019-09-27 10:59:30 -0700

    modularized code and created two different student sheets so user could exchange out student roster full list or by strength level easily and just run program

---
+++ students-roster/student_full-roster.js
@@ -0,0 +1,21 @@
+const students = [
+    'person1',
+    'person2',
+    'person3',
+    'person4',
+    'person5',
+    'person6',
+    'person7',
+    'person8',
+    'person9',
+    'person10',
+    'person11',
+    'person12',
+    'person14',
+    'person15',
+    'person16',
+    'person17',
+    'person18'
+];
+
+module.exports = students;
